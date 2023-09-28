ee95706e865cb4b73f8c844a54a3b8bd26293e24
GrumpyMonka <40570589+GrumpyMonka@users.noreply.github.com>
2023-09-28 20:53:08 +0300

    Добавил комментарии в код

--- Task_DB/DBEditor/dbview.cpp
+++ Task_DB/DBEditor/dbview.cpp
@@ -16,10 +16,13 @@
 DBView::DBView( QWidget* parent )
     : QTableView( parent )
 {
+    // Создание модели
     sqlModel = new QStandardItemModel( this );
     setModel( sqlModel );
     connect( sqlModel, &QStandardItemModel::itemChanged,
         this, &DBView::itemChanged );
+
+    // Магия, чтобы столбцы растягивались на всю таблицу, а не теснились слева
     this->horizontalHeader()->setSectionResizeMode( QHeaderView::Stretch );
 }
 
@@ -30,6 +33,7 @@ DBView::~DBView()
 
 void DBView::LoadDataFormDB( const QString& filter )
 {
+    // Установка названия столбцов
     sqlModel->clear();
     sqlModel->setHorizontalHeaderLabels( QStringList() << "Перфикс"
                                                        << "Название"
@@ -40,16 +44,19 @@ void DBView::LoadDataFormDB( const QString& filter )
                                                        << "Долгота"
                                                        << "Описание" );
 
+    // Запрос для получения всех записей, удовлетворяющих фильтру
     QSqlQuery sqlQuery;
     if ( sqlQuery.exec( QString( QUERY_GET_ALL ).arg( filter ) ) )
     {
         while ( sqlQuery.next() )
         {
+            // Получение записей и занесение в модель таблицы
             QList<QStandardItem*> itemList;
             for ( auto column : COLUMNS_NEW )
             {
                 itemList.append( new QStandardItem( sqlQuery.value( column ).toString() ) );
             }
+            // Скрытая колонка с id, чтобы иметь возможность нормально обращаться к БД
             itemList.append( new QStandardItem( sqlQuery.value( DB_TABLE_PK_NAME ).toString() ) );
             sqlModel->appendRow( itemList );
         }
@@ -58,12 +65,14 @@ void DBView::LoadDataFormDB( const QString& filter )
     {
         QMessageBox::warning( this, this->metaObject()->className(), QString( "Ошибка загрузки данных из БД. %1" ).arg( sqlQuery.lastError().text() ) );
     }
+    // Скрываем столбец с id
     this->hideColumn( sqlModel->columnCount() - 1 );
     emit dbLoaded( sqlModel->rowCount() );
 }
 
 void DBView::LoadDB()
 {
+    // Грузим БД
     if ( !QFile::exists( DB_PATH ) )
     {
         QMessageBox::warning( this, this->metaObject()->className(), QString( "База Данных не найдена!\n %1" ).arg( DB_PATH ) );
@@ -83,6 +92,7 @@ void DBView::LoadDB()
 
 void DBView::addRecord()
 {
+    // Запрос на добавление пустой строки
     QSqlQuery sqlQuery;
     sqlQuery.prepare( QUERY_INSERT );
     for ( auto value : COLUMNS )
@@ -90,6 +100,7 @@ void DBView::addRecord()
         sqlQuery.bindValue( ":" + value, "" );
     }
 
+    // Если запрос отработал успешно, то добавляем в модель таблицы
     if ( sqlQuery.exec() )
     {
         QList<QStandardItem*> itemList;
@@ -97,6 +108,7 @@ void DBView::addRecord()
         {
             itemList.append( new QStandardItem( "" ) );
         }
+        // Получаем id добавленной записи
         itemList.append( new QStandardItem( sqlQuery.lastInsertId().toString() ) );
         sqlModel->appendRow( itemList );
         this->selectionModel()->select( itemList.first()->index(), QItemSelectionModel::ClearAndSelect );
@@ -111,6 +123,7 @@ void DBView::addRecord()
 
 void DBView::removeRecord()
 {
+    // Получаем индексы для удаления
     QStringList indexes;
     auto selectedRows = this->selectionModel()->selectedIndexes();
     for ( auto index : selectedRows )
@@ -118,6 +131,7 @@ void DBView::removeRecord()
         indexes.push_back( sqlModel->item( index.row(), sqlModel->columnCount() - 1 )->text() );
     }
 
+    // Отправляем запрос на удаление и, если он прошёл, то удаляем эти элементы из таблицы
     QSqlQuery sqlQuery;
     if ( sqlQuery.exec( QString( QUERY_REMOVE ).arg( indexes.join( ", " ) ) ) )
     {
@@ -135,7 +149,10 @@ void DBView::removeRecord()
 
 void DBView::itemChanged( QStandardItem* item )
 {
+    // Получаем название полонки в которой произошло изменение
     QString name_column = COLUMNS.at( item->column() );
+
+    // Составляем запрос на изменение
     QSqlQuery sqlQuery;
     QString value = item->text();
     if ( "population" == name_column )
@@ -147,6 +164,7 @@ void DBView::itemChanged( QStandardItem* item )
     sqlQuery.bindValue( ":" + name_column, value );
     if ( sqlQuery.exec() )
     {
+        // Пусть будет
     }
     else
     {
@@ -156,5 +174,6 @@ void DBView::itemChanged( QStandardItem* item )
 
 void DBView::setFilter( const QString& filter )
 {
+    // Применение фильтра
     LoadDataFormDB( filter );
 }
--- Task_DB/DBEditor/mainwindow.cpp
+++ Task_DB/DBEditor/mainwindow.cpp
@@ -20,27 +20,35 @@ MainWindow::MainWindow( QWidget* parent )
 
 void MainWindow::CreateUI()
 {
+    // Устанавливаем название приложения
     this->setWindowTitle( NAME_APP );
+
+    // Создаём основной лайаут на который будем всё накидывать
     auto gridLayout = new QGridLayout();
     {
+        // Надпись
         labelCountRecords = new QLabel( "Кол-во записей: 0" );
         labelCountRecords->setMaximumHeight( 25 );
 
+        // Таблица БД ( обьявлена после лейбла, потому как при старте LoadDB() изменит значение, а вызывать  LoadDB() ниже не хочется )
         dbView = new DBView( this );
         connect( dbView, &DBView::dbLoaded,
             this, &MainWindow::on_dbLoaded );
         dbView->LoadDB();
 
+        // Кнопка для добавления записи в таблицу
         QPushButton* buttonAddRecord = new QPushButton( "Добавить", this );
         buttonAddRecord->setMinimumWidth( 60 );
         connect( buttonAddRecord, &QPushButton::clicked,
             dbView, &DBView::addRecord );
 
+        // Добавление кнопки для удаления записей из таблицы
         QPushButton* buttonRemoveRecord = new QPushButton( "Удалить", this );
         buttonRemoveRecord->setMinimumWidth( 60 );
         connect( buttonRemoveRecord, &QPushButton::clicked,
             dbView, &DBView::removeRecord );
 
+        // Фильтр для населения
         comboBox = new QComboBox( this );
         comboBox->addItems( QStringList() << "Население не учитывать."
                                           << "Население от 1 тыс. чел."
@@ -52,20 +60,23 @@ void MainWindow::CreateUI()
                                           << "Население от 500 тыс. чел."
                                           << "Население от 1 млн. чел."
                                           << "Население от 2 млн. чел." );
-
         connect( comboBox, &QComboBox::currentTextChanged,
             this, &MainWindow::comboBoxItemChanged );
 
+        // Надпись для фильтра
         QLabel* labelFilter = new QLabel( "Фильтр SQL:", this );
         labelFilter->setMaximumHeight( 25 );
 
+        // Поле фильтра
         textEditFilter = new QTextEdit( this );
         textEditFilter->setSizePolicy( QSizePolicy::Maximum, QSizePolicy::Expanding );
 
+        // Кнопка для применения фильтра
         QPushButton* buttonFilter = new QPushButton( "Применить фильтр", this );
         connect( buttonFilter, &QPushButton::clicked,
             this, &MainWindow::filterChanged );
 
+        // Накидывание элементов на форму
         gridLayout->addWidget( dbView, 0, 0, 10, 1 );
         gridLayout->addWidget( labelCountRecords, 0, 1 );
         gridLayout->addWidget( buttonAddRecord, 1, 1 );
@@ -75,6 +86,8 @@ void MainWindow::CreateUI()
         gridLayout->addWidget( textEditFilter, 5, 1 );
         gridLayout->addWidget( buttonFilter, 6, 1 );
     }
+
+    // Установаливаем лайаут на форму
     auto widget = new QWidget( this );
     widget->setLayout( gridLayout );
     this->setCentralWidget( widget );
@@ -82,16 +95,19 @@ void MainWindow::CreateUI()
 
 void MainWindow::comboBoxItemChanged()
 {
+    // Фильтра на население
     dbView->setFilter( "population>=" + QString::number( filter_population[comboBox->currentIndex()] * 1000 ) );
 }
 
 void MainWindow::on_dbLoaded( int countRecords )
 {
+    // Вывод кол-ва найденных записей
     labelCountRecords->setText( "Кол-во записей: " + QString::number( countRecords ) );
 }
 
 void MainWindow::filterChanged()
 {
+    // Фильтр пользовательский + фильтр населения
     QString filter = "population>=" + QString::number( filter_population[comboBox->currentIndex()] * 1000 );
     if ( !textEditFilter->toPlainText().isEmpty() )
     {
